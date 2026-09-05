c642bc6061f0a834ae8bb8e10124aaa6ea753571
raihanirfan <irfanraihanal@gmail.com>
2026-09-06 00:29:26 +0700

    drop: cuty.io from website tags + check.js (userscript already 0.2.19)

--- functions/api/check.js
+++ functions/api/check.js
@@ -6,7 +6,7 @@ const MAP=[
   {re:/safelink/i, name:"Safelink"},
   {re:/shorte\.st|sh\.st/i, name:"Shorte.st"},
   {re:/linkvertise|link-to\.net/i, name:"Linkvertise"},
-  {re:/work\.ink|bstlar\.com|lootlinks|loot-link|cuty\.io|paster\.so/i, name:"AdLink"},
+  {re:/work\.ink|bstlar\.com|lootlinks|loot-link|paster\.so/i, name:"AdLink"},
   {re:/bit\.ly|cutt\.ly|rebrand\.ly|shorter\.me|t\.ly|tiny\.cc|tinyurl\.com|shorturl\.at/i, name:"Shortener"},
 ];
 export async function onRequestPost({request}){
